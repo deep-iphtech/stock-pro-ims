13f1ab64643483e4795f9c7c44ae0d2508efacef
Deep Chaturvedi <deep.chaturvedi@iphtechnologies.com>
2026-07-16 13:44:12 +0530

    Change inventory reponse to datatable

--- dist/services/apd_inventory/inventory.service.d.ts
+++ dist/services/apd_inventory/inventory.service.d.ts
@@ -2,7 +2,12 @@ import { Inventory } from "../../models/Inventory.js";
 import { BaseService } from "../base/base.service.js";
 export declare class InventoryService extends BaseService<Inventory> {
     constructor();
-    getAllProductsInventory(): Promise<Inventory[]>;
+    getAllProductsInventory(query: Record<string, unknown>): Promise<{
+        draw: number;
+        recordsTotal: number;
+        recordsFiltered: number;
+        data: Inventory[];
+    }>;
     findByWarehouse(warehouseId: number): Promise<Inventory[]>;
     findByProduct(productId: number): Promise<Inventory[]>;
     findByProductAndWarehouse(productId: number, warehouseId: number): Promise<Inventory | null>;
