efae4b016881459a582a386d8cacbfd544255737
Deep Chaturvedi <deep.chaturvedi@iphtechnologies.com>
2026-07-17 12:42:07 +0530

    updated payload for adjust stock

--- dist/validations/inventory.schema.d.ts
+++ dist/validations/inventory.schema.d.ts
@@ -1,12 +1,9 @@
 import { z } from "zod";
-export declare const inventoryCuSchema: z.ZodObject<{
-    product_id: z.ZodNumber;
-    warehouse_id: z.ZodOptional<z.ZodNullable<z.ZodNumber>>;
-    available_qty: z.ZodOptional<z.ZodNumber>;
-    reserved_qty: z.ZodOptional<z.ZodNumber>;
-}, z.core.$strict>;
 export declare const adjustStockSchema: z.ZodObject<{
-    warehouseId: z.ZodCoercedNumber<unknown>;
-    productId: z.ZodCoercedNumber<unknown>;
-    quantity: z.ZodCoercedNumber<unknown>;
-}, z.core.$strip>;
+    product_id: z.ZodNumber;
+    inv: z.ZodArray<z.ZodObject<{
+        warehouse_id: z.ZodNumber;
+        available_qty: z.ZodNumber;
+        reserved_qty: z.ZodOptional<z.ZodNumber>;
+    }, z.core.$strip>>;
+}, z.core.$strict>;
